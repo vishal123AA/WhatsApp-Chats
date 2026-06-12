6d9dd899d9d194db8bc261bf29f00fee6d575a87
vishal <vishalkumarkalher16@gmail.com>
2026-06-12 15:08:22 +0530

    in this we created the route for /chats to see all the chats

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 const mongoose = require('mongoose');
+const Chat = require('./models/chat');
 
 main()
     .then(() => {
@@ -27,9 +28,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-
-
-
+app.get('/chats', async(req, res) => {
+    let chats = await Chat.find()
+    res.render('index.ejs', { chats });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
